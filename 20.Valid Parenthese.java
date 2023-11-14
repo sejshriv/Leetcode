181c885494ea71ade0f4e54a2f08d1db2afb84b9
sejshriv <120376257+sejshriv@users.noreply.github.com>
2023-11-14 22:35:49 +0530

    Create 20.Valid Parenthese.java
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.

---
+++ 20.Valid Parenthese.java
@@ -0,0 +1,32 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>(); // create an empty stack
+        for (char c : s.toCharArray()){
+            if(c =='('){
+                stack.push(')');
+            }
+            else if( c =='{'){
+                stack.push('}');
+            }
+            else if( c =='['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != c)
+            {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
